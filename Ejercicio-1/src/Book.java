a4c6a082f38001306fc0ca709263ffb33886b833
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-08 12:16:01 +0200

    refactor: adding guard clause for field validation

--- Ejercicio-1/src/Book.java
+++ Ejercicio-1/src/Book.java
@@ -2,6 +2,15 @@ public class Book {
     private String name;
 
     public Book(String name){
+
+        if (name==(null)){
+            throw new IllegalArgumentException("The name of the book can't be null");
+        }
+
+        if (name.isBlank()){
+            throw new IllegalArgumentException("The name of the book can't be empty");
+        }
+
         this.name=name;
     }
 
