0e58b80ba6360026ee8a74a87d9efb377f706f19
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-14 09:08:36 +0200

    feature: we add the ethod for getting the verificationletter

---
+++ Ejercicio2/src/main/java/DNICalculator.java
@@ -0,0 +1,29 @@
+/*Crea una classe anomenada CalculoDni que calculi la lletra del DNI en rebre el número com a paràmetre.
+Crea una classe jUnit que verifiqui el seu correcte funcionament, parametritzant-la perquè el test rebi
+un espectre de dades ampli i validi si el càlcul és correcte per a 10 números de DNI predefinits.*/
+
+import java.util.List;
+
+public class DNICalculator {
+
+    public static void main(String[] args) {
+
+        //47421215Y
+       getDNILetter(47421215);
+
+
+    }
+
+static Character getDNILetter(int dni) {
+
+    int verification=dni%23;
+
+    List<Character> lettersDni = List.of(
+            'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+            'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'
+    );
+System.out.println("Your verification letter is: "+lettersDni.get(verification));
+    return lettersDni.get(verification);
+
+}
+}
---
+++ Ejercicio2/src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Type in DNI number without the letter");
+        //47421215Y
+        DNICalculator.getDNILetter(47421215);
+
+
+    }
+
+
+}
