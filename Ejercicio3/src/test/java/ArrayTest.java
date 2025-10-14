6ddebe0a67653e7b1010569042bc4d3433414e7e
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-14 11:02:46 +0200

    feat: adding throw test

--- Ejercicio3/src/test/java/ArrayTest.java
+++ Ejercicio3/src/test/java/ArrayTest.java
@@ -7,14 +7,9 @@ class ArrayTest {
     @Test
     void testArrayIndexOutOfBounds() {
 
-        // 1. Especifica el TIPO de excepción que esperas:
         Class<ArrayIndexOutOfBoundsException> expectedException = ArrayIndexOutOfBoundsException.class;
-
-        // 2. Llama a assertThrows, pasándole la excepción esperada y la LÓGICA que la provoca.
-
-        // ¿Cómo llamarías a assertThrows usando una expresión lambda (->) para ejecutar ArrayHandler.getFruit()?
-
-        // assertThrows( expectedException, () -> { /* TU CÓDIGO AQUÍ */ } );
+        assertThrows(expectedException, ()->{Array.getFruit(3);});
+        
 
     }
 
