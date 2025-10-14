6638e1fbc7ae0a36c0e3c9332537559e90f1d23a
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-14 09:13:50 +0200

    refactor: delete Main and adding dni numbers for testing purposes

--- Ejercicio2/src/test/java/DNICalculatorTest.java
+++ Ejercicio2/src/test/java/DNICalculatorTest.java
@@ -1,5 +1,16 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class DNICalculatorTest {
+    32096242H
+            X1485306N
+            11952101J
+    72541408Y
+    15523720P
+    43016843N
+    74493388E
+            35851198V
+            X4024762S
+    Y9479487M
+
 
 }
