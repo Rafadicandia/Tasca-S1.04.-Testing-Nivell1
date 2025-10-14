212b61b9c14a57f6c32fd8dbd88eb0e5f12b3346
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-14 10:37:06 +0200

    reafactor: we add the parameterizedTEst method

--- Ejercicio2/src/main/java/DNICalculator.java
+++ Ejercicio2/src/main/java/DNICalculator.java
@@ -6,13 +6,6 @@ import java.util.List;
 
 public class DNICalculator {
 
-    public static void main(String[] args) {
-
-
-       getDNILetter(32096242);
-
-
-    }
 
 static Character getDNILetter(int dni) {
 
@@ -22,7 +15,6 @@ static Character getDNILetter(int dni) {
             'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
             'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'
     );
-System.out.println("Your verification letter is: "+lettersDni.get(verification));
     return lettersDni.get(verification);
 
 }
--- Ejercicio2/src/test/java/DNICalculatorTest.java
+++ Ejercicio2/src/test/java/DNICalculatorTest.java
@@ -1,16 +1,39 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest; // ¡Necesitas este import!
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 class DNICalculatorTest {
-    32096242H
-            X1485306N
-            11952101J
-    72541408Y
-    15523720P
-    43016843N
-    74493388E
-            35851198V
-            X4024762S
-    Y9479487M
+
+//    @Test
+//    public void DNILetterTest() {
+//        int dni = 32096242;
+//        char letter='H';
+//        char calculated=DNICalculator.getDNILetter(dni);
+//        assertEquals(letter, calculated, "Not Correct");
+//    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "32096242, H",
+            "11952101, J",
+            "17384737, A",
+            "72541408, Y",
+            "15523720, P",
+            "43016843, N",
+            "74493388, E",
+            "35851198, V",
+            "32812751, P",
+            "12950433, F"
+    })
+    public void DNILetterTest(int dni, char letter){
+
+        char calculated=DNICalculator.getDNILetter(dni);
+        assertEquals(letter, calculated, "Not Correct");
+
+    }
+
 
 
 }
